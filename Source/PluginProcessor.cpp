aff2e45ba2b3ed9c7655d2a905b5bb690de1ffab
IvanCisco <munozivan76@yahoo.it>
2025-03-25 09:07:12 +0100

    Corrections

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -150,7 +150,6 @@ void StoneMistressAudioProcessor::setStateInformation (const void* data, int siz
 
 void StoneMistressAudioProcessor::parameterChanged(const String& paramID, float newValue)
 {
-    DBG("Parameter Changed!");
     if (paramID == Parameters::nameRate)
     {
         lfo.setRate(newValue);
